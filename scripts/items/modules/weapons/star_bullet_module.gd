afed6eabd84b90d8e5a284ba2b22f8243b9f1f86
yashparab17 <yash.sp@somaiya.edu>
2025-03-19 22:46:29 +0530

    Item pickup effect.

--- scripts/items/modules/weapons/star_bullet_module.gd
+++ scripts/items/modules/weapons/star_bullet_module.gd
@@ -1,12 +1,22 @@
-extends Node2D
+extends AnimatedSprite2D
 
 # The type of module.
 @export var module_type: String = "star_bullet"
+
+# Item pickup effect.
+@onready var item_pickup = preload("res://scenes/effects/item_pickup.tscn")
 
 func _on_pickup_area_body_entered(body: Node2D) -> void:
 	# Check if the body that entered is the player.
 	if body.is_in_group("player"):
 		# Call the player's method to collect the module.
 		body.collect_module(module_type)
+		
+		
+		# Spawn the item pickup effect.
+		var item_pickup_instance = item_pickup.instantiate() as Node2D
+		item_pickup_instance.global_position = global_position
+		get_parent().add_child(item_pickup_instance)
+		
 		# Remove the module from the scene.
 		queue_free()
--- scripts/items/pickups/health_pickup.gd
+++ scripts/items/pickups/health_pickup.gd
@@ -4,7 +4,7 @@ extends AnimatedSprite2D
 @onready var snd_health_pickup = $Sounds/Pickup
 
 # Handles collision when the health pickup hits a body.
-func _on_area_body_entered(body: Node2D) -> void:
+func _on_pickup_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		if HealthManager.current_health == 3:
 			pass
