d5bcdda45f62475e436651f39ced3493660f23c3
yashparab17 <yash.sp@somaiya.edu>
2025-03-30 22:00:34 +0530

    Camera smoothing.

--- scenes/characters/player/player_camera.gd
+++ scenes/characters/player/player_camera.gd
@@ -6,3 +6,7 @@ extends Camera2D
 func _process(delta: float) -> void:
 	if player != null:
 		global_position = player.global_position
+		if Input.is_action_pressed("aim_up"):
+			global_position = global_position + Vector2(0, -64)
+		if Input.is_action_pressed("aim_down"):
+			global_position = global_position + Vector2(0, 64)
