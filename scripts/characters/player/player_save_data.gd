d4d9b52e63fdcc336491711c3be578287437f838
yashparab17 <yash.sp@somaiya.edu>
2025-04-05 23:09:24 +0530

    Mai pagal ho chuka hoon

--- scripts/characters/player/player_save_data.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name PlayerSaveData
-
-@export var player_position: Vector2 = Vector2.ZERO
-@export var current_health: int = 3
-@export var max_health: int = 3
-@export var unlocked_weapons: Array[String] = []
-@export var current_weapon: String = ""
-@export var scene_path: String = ""
