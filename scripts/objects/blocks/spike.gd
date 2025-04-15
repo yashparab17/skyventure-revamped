71dc857e82f6f64d4b2b6296a44aa3924af43be5
yashparab17 <yash.sp@somaiya.edu>
2025-04-15 23:10:44 +0530

    Update spike.

--- scripts/objects/blocks/spike.gd
+++ scripts/objects/blocks/spike.gd
@@ -1,5 +1,19 @@
 extends StaticBody2D
 
+# Direction of the spike.
+@export_enum("Up", "Right", "Down", "Left") var facing_direction: String = "Up"
+
+func _ready() -> void:
+	match facing_direction:
+		"Up":
+			rotation_degrees = 0
+		"Right":
+			rotation_degrees = 90
+		"Down":
+			rotation_degrees = 180
+		"Left":
+			rotation_degrees = 270
+
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.take_damage(1, global_position.x)
